278cc8c368f45aa06e059b827574a3aaf2e986a9
Edinson Ayui <edison.ayui@gmail.com>
2025-03-10 20:41:33 -0500

    add: make new map

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,12 +10,15 @@ export class Game extends Scene {
 
     private scaleSize: number = 1;
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
-    private frameRate: number = 10;
+    private frameRate: number = 5;
 
     private layerBackground: Phaser.Tilemaps.TilemapLayer | null | undefined;
     private layerDecoratorUp: Phaser.Tilemaps.TilemapLayer | null | undefined;
     private layerDecoratorBack: Phaser.Tilemaps.TilemapLayer | null | undefined;
+
+    private playerContainer!: Phaser.GameObjects.Container;
     private player: Phaser.Physics.Arcade.Sprite;
+    private weapon!: Phaser.GameObjects.Sprite;
 
     constructor() {
         super('Game');
@@ -24,10 +27,10 @@ export class Game extends Scene {
 
     preload() {
         this.load.setPath('assets');
-        // Cargar la hoja de sprites (tileset)
-        this.load.tilemapTiledJSON('map', 'maps/desert/desert.json');
-        this.load.image('mountain', 'maps/desert/mountain_landscape.png');
+        this.load.tilemapTiledJSON('map', 'maps/castle/castle.json');
+        this.load.image('Castle2', 'maps/castle/Castle2.png');
         this.load.spritesheet('player', 'the_knight.png', { frameWidth: this.spriteSize, frameHeight: this.spriteSize });
+        this.load.spritesheet('weapon', 'weapon-sword.png', { frameWidth: this.spriteSize, frameHeight: this.spriteSize });
     }
 
     create() {
@@ -36,7 +39,7 @@ export class Game extends Scene {
 
         const map = this.make.tilemap({ key: 'map' });
 
-        const tilesetMountain = map.addTilesetImage('mountain_landscape', 'mountain');
+        const tilesetMountain = map.addTilesetImage('Castle2', 'Castle2');
 
         if (!tilesetMountain) {
             return;
@@ -53,15 +56,23 @@ export class Game extends Scene {
             return;
         }
 
-        this.player = this.physics.add.sprite(centerX, centerY, 'player').setScale(this.scaleSize);
+        this.player = this.physics.add.sprite(this.spriteSize / 2, this.spriteSize / 2, 'player').setScale(this.scaleSize);
+        this.weapon = this.physics.add.sprite(this.spriteSize / 2, this.spriteSize / 2 + 8, 'weapon').setScale(this.scaleSize);
 
-        this.player.setCollideWorldBounds(true);
-        this.cameras.main.startFollow(this.player);
+        this.playerContainer = this.add.container(centerX - this.spriteSize / 2, centerY - this.spriteSize / 2, [this.player, this.weapon]);
+        this.physics.world.enable(this.playerContainer);
+        
+        const container = this.playerContainer.body as Phaser.Physics.Arcade.Body;
+
+        container.setSize(this.spriteSize, this.spriteSize);
+        container.setCollideWorldBounds(true);
+
+        this.cameras.main.startFollow(this.playerContainer);
 
         this.layerBackground.setDepth(0);
         this.layerDecoratorBack.setDepth(1);
-        this.player.setDepth(2);
-        this.layerDecoratorUp.setDepth(3);
+        this.playerContainer.setDepth(2);
+        this.layerDecoratorUp.setDepth(4);
 
         const collisionObjects = map.getObjectLayer('collisions');
 
@@ -82,19 +93,70 @@ export class Game extends Scene {
             });
 
             // Detectar colisiones entre el jugador y las áreas
-            this.physics.add.collider(this.player, collisionGroup);
+            this.physics.add.collider(this.playerContainer, collisionGroup);
         }
 
 
 
         if (this.input.keyboard) {
             this.cursors = this.input.keyboard.createCursorKeys();
-            this.makeCharacter(this.anims)
+            this.animateCharacter(this.anims)
+            this.animateWeapon(this.anims);
         }
 
     }
 
-    makeCharacter(anims: Phaser.Animations.AnimationManager) {
+    animateWeapon(anims: Phaser.Animations.AnimationManager) {
+        anims.create({
+            key: 'weapon-down',
+            frames: [
+                { key: 'weapon', frame: 0 },
+                { key: 'weapon', frame: 1 },
+                { key: 'weapon', frame: 0 },
+                { key: 'weapon', frame: 1 },
+            ],
+            frameRate: this.frameRate,
+            repeat: -1,
+        });
+
+        anims.create({
+            key: 'weapon-up',
+            frames: [
+                { key: 'weapon', frame: 2 },
+                { key: 'weapon', frame: 3 },
+                { key: 'weapon', frame: 2 },
+                { key: 'weapon', frame: 3 },
+            ],
+            frameRate: this.frameRate,
+            repeat: -1,
+        });
+
+        anims.create({
+            key: 'weapon-right',
+            frames: [
+                { key: 'weapon', frame: 4 },
+                { key: 'weapon', frame: 5 },
+                { key: 'weapon', frame: 4 },
+                { key: 'weapon', frame: 6 },
+            ],
+            frameRate: this.frameRate,
+            repeat: -1,
+        });
+
+        anims.create({
+            key: 'weapon-left',
+            frames: [
+                { key: 'weapon', frame: 4 },
+                { key: 'weapon', frame: 5 },
+                { key: 'weapon', frame: 4 },
+                { key: 'weapon', frame: 6 },
+            ],
+            frameRate: this.frameRate,
+            repeat: -1,
+        });
+    }
+
+    animateCharacter(anims: Phaser.Animations.AnimationManager) {
         anims.create({
             key: 'walk-down',
             frames: [
@@ -147,21 +209,7 @@ export class Game extends Scene {
 
     update(): void {
         this.moveCharacter();
-        this.moveMap();
     }
-
-    moveMap(): void {
-        if (this.cursors.left.isDown) {
-            this.cameras.main.scrollX -= this.velocityX;
-        } else if (this.cursors.right.isDown) {
-            this.cameras.main.scrollX += this.velocityX;
-        } else if (this.cursors.up.isDown) {
-            this.cameras.main.scrollY -= this.velocityY;
-        } else if (this.cursors.down.isDown) {
-            this.cameras.main.scrollY += this.velocityY;
-        }
-    }
-
 
     moveCharacter(): void {
 
@@ -182,7 +230,7 @@ export class Game extends Scene {
             }
             isPressedR1 = pad.R1;
         }
-        
+
         if (this.cursors.shift.isDown || isPressedR1 === 1) {
             run = 100;
         } else {
@@ -193,27 +241,54 @@ export class Game extends Scene {
         if (this.cursors.left.isDown || axisH < 0) {
             velocityX += -this.velocityX - run;
             this.player.play('walk-left', true);
+            this.weapon.play('weapon-left', true);
         } else if (this.cursors.right.isDown || axisH > 0) {
             velocityX += this.velocityX + run;
             this.player.play('walk-right', true);
+            this.weapon.play('weapon-right', true);
         } else if (this.cursors.up.isDown || axisV < 0) {
             velocityY += -this.velocityY - run;
             this.player.play('walk-up', true);
+            this.weapon.play('weapon-up', true);
         } else if (this.cursors.down.isDown || axisV > 0) {
             velocityY += this.velocityY + run;
             this.player.play('walk-down', true);
+            this.weapon.play('weapon-down', true);
         } else if ((velocityX === 0 && velocityY === 0) && (axisV == 0 && axisV == 0)) {
             this.player.stop();
+            this.weapon.stop();
         }
 
         this.onMoveCharacter(velocityX, velocityY);
     }
 
     onMoveCharacter(velocityX: number, velocityY: number) {
+        const container = this.playerContainer.body as Phaser.Physics.Arcade.Body;
 
-        this.player.setVelocity(velocityX, velocityY);
+        // Mueve el contenedor
+        container.setVelocity(velocityX, velocityY);
+
+        // Actualiza las animaciones según la dirección
+        if (velocityX < 0) {
+            this.player.play('walk-left', true);
+            this.weapon.play('weapon-left', true);
+        } else if (velocityX > 0) {
+            this.player.play('walk-right', true);
+            this.weapon.play('weapon-right', true);
+        } else if (velocityY < 0) {
+            this.player.play('walk-up', true);
+            this.weapon.play('weapon-up', true);
+        } else if (velocityY > 0) {
+            this.player.play('walk-down', true);
+            this.weapon.play('weapon-down', true);
+        } else {
+            this.player.stop();
+            this.weapon.stop();
+        }
+
 
         const currentFrame = this.player.anims.currentFrame?.index;
+        const currentFrameWeapon = this.weapon.anims.currentFrame?.index;
 
         if (this.player.anims.currentAnim?.key === 'walk-down' || this.player.anims.currentAnim?.key === 'walk-up') {
             if (currentFrame === 2) {
@@ -231,6 +306,37 @@ export class Game extends Scene {
             this.player.setFlipX(true);
         }
 
-    }
+        if (this.weapon.anims.currentAnim?.key === 'weapon-up' || this.weapon.anims.currentAnim?.key === 'weapon-down') {
+            this.weapon.setFlipX(false);
+            this.weapon.setPosition(this.player.x, this.player.y + 8);
+        }
 
+        if (this.weapon.anims.currentAnim?.key === 'weapon-right') {
+            this.weapon.setFlipX(false);
+            if (currentFrameWeapon === 1 || currentFrameWeapon === 3) {
+                this.weapon.setPosition(this.player.x + 8, this.player.y);
+            }
+            if (currentFrameWeapon === 2) {
+                this.weapon.setPosition(this.player.x + 12, this.player.y);
+            }
+            if (currentFrameWeapon === 4) {
+                this.weapon.setPosition(this.player.x + 5, this.player.y);
+            }
+        }
+
+        if (this.weapon.anims.currentAnim?.key === 'weapon-left') {
+            this.weapon.setFlipX(true);
+            if (currentFrameWeapon === 1 || currentFrameWeapon === 3) {
+                this.weapon.setPosition(this.player.x - 8, this.player.y);
+            }
+            if (currentFrameWeapon === 2) {
+                this.weapon.setPosition(this.player.x - 12, this.player.y);
+            }
+            if (currentFrameWeapon === 4) {
+                this.weapon.setPosition(this.player.x - 5, this.player.y);
+            }
+        }
+
+
+    }
 }
